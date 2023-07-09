6c82f953edb76f0ea93a927a8551446b3ed1a3e3
7kanak <kanak.shri07@gmail.com>
2023-07-09 12:53:32 +0530

    updates

--- src/main.py
+++ src/main.py
@@ -1,27 +1,34 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from model_base import SiameseNetwork
+from model_base import SiameseNetwork, predict
 from sampler_base import CustomSampler
 from dataset_base import SiameseDataset
 from loss import construct_loss
+from torch.utils.data import Subset, random_split
 
 
 
 
 def get_loaders(dataset, model, cluster_size, batch_size):
     assert cluster_size <= batch_size//2
-    sampler = CustomSampler(dataset, model, cluster_size, batch_size)
-    train_dataloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler)
-    val_dataloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler) # todo: change
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+
+    sampler = CustomSampler(train_dataset, model, cluster_size, batch_size)
+    train_dataloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler) # todo: update this
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_sampler=sampler) # todo: change
     return train_dataloader, val_dataloader
 
 def get_cluster_size(curr_cluster_size, epoch, batch_size):
     return curr_cluster_size
 
-def train_siamese_model():
-    
-    num_epochs = 10
+def train_siamese_model(input_dataset):
+    num_epochs = 1
     learning_rate = 0.001
     batch_size = 16
     cluster_size = 4
@@ -34,7 +41,7 @@ def train_siamese_model():
     criteria = construct_loss()
     model.to(device)
     
-    sd = SiameseDataset("old_files/adbase_indexed_06062023.csv",columns= ("ML_Transcripted_text", "Actual_product"))
+    
     train_dataloader, val_dataloader = None, None
 
     history = {"train_loss": [], "val_loss": []}
@@ -44,7 +51,7 @@ def train_siamese_model():
         if epoch%refresh_interval==0:
             print("Refreshing clusters....")
             cluster_size = get_cluster_size(cluster_size, epoch, batch_size)
-            train_dataloader, val_dataloader = get_loaders(sd, model, cluster_size=cluster_size, batch_size=batch_size)
+            train_dataloader, val_dataloader = get_loaders(input_dataset, model, cluster_size=cluster_size, batch_size=batch_size)
 
         train_loss = 0.0
         val_loss = 0.0
@@ -60,20 +67,34 @@ def train_siamese_model():
         train_loss /= len(train_dataloader)
         history["train_loss"].append(train_loss)
     
-        model.eval()
-        with torch.no_grad():
-            for batch in val_dataloader:
-                loss = model(batch)
-                val_loss += loss.item()
+        # model.eval()
+        # with torch.no_grad():
+        #     for batch in val_dataloader:
+        #         sim, target = model(batch)
+        #         loss = criteria(input=sim, target=target)
+        #         val_loss += loss.item()
 
-            val_loss /= len(val_dataloader)
-            history["val_loss"].append(val_loss)
-        print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f} - Val Loss: {val_loss:.4f}")
+        #     val_loss /= len(val_dataloader)
+        #     history["val_loss"].append(val_loss)
+        # print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f} - Val Loss: {val_loss:.4f}")
 
     print("Training completed.")
+    return model
 
 def main():
-    train_siamese_model()
+    sd = SiameseDataset("old_files/adbase_indexed_06062023.csv",columns= ("ML_Transcripted_text", "Actual_product"))
+    model = train_siamese_model(sd)
+    
+    import pandas as pd
+    test_data = pd.read_csv("old_files/adbase_indexed_06062023.csv").sample(100)
+    test_data = test_data.dropna()
+    test_data_list = test_data["ML_Transcripted_text"].tolist()
+    true_val = test_data["Actual_product"].tolist()
+
+    all_labels = pd.read_csv("old_files/adbase_indexed_06062023.csv")["Actual_product"].tolist()
+
+
+    predict(model, test_data_list, all_labels, true_val)
 
 
 if __name__ == "__main__":
--- src/model_base.py
+++ src/model_base.py
@@ -24,6 +24,7 @@ class TextEncoder(nn.Module):
         embeddings = outputs.last_hidden_state[:, 0, :]  # Extract embeddings from the [CLS] token
         embeddings = self.adaptive_pool(embeddings)
         embeddings = embeddings.unsqueeze(1)
+        embeddings = F.normalize(embeddings , p=2, dim=-1)
         return embeddings
     
 
@@ -41,12 +42,38 @@ class SiameseNetwork(torch.nn.Module):
         x_hat_1 = self.text_model(xt)
         y_hat_1 = self.text_model(yt)
 
-        x_hat_1 = F.normalize(x_hat_1 , p=2, dim=-1)
-        y_hat_1 = F.normalize(y_hat_1 , p=2, dim=-1)
+        # x_hat_1 = F.normalize(x_hat_1 , p=2, dim=-1)
+        # y_hat_1 = F.normalize(y_hat_1 , p=2, dim=-1)
 
         similarity = x_hat_1.squeeze() @ y_hat_1.squeeze().T
 
         target = torch.eye(similarity.shape[0]).to(self.device)
         return similarity, target
 
+import numpy as np
+def predict(model: SiameseNetwork, input_document: list, all_labels: list, true_val): # todo: cache all_labels_rep
+    model.eval()
+    model.text_model.eval()
+    model = model.cpu()
+    model.text_model = model.text_model.cpu()
+    model.device = "cpu"
+    model.text_model.device = "cpu"
+    with torch.no_grad():
+        input_document = [i for i in input_document if isinstance(i, str)]
+        input_doc_rep = model.text_model(input_document) # eg. if prediction for 100 doc, then of size 100,1
+        all_labels = [i for i in all_labels if isinstance(i, str)]
 
+        all_labels_rep = []
+        chunks = [all_labels[i:i+100] for i in range(0, len(all_labels), 100)]
+        for i in chunks:
+            tmp_rep = model.text_model(i) # eg. if 1000 total labels then of size 1000,1
+            all_labels_rep.append(tmp_rep.squeeze().numpy())
+        all_labels_rep = np.vstack(all_labels_rep)
+        all_labels_rep = torch.Tensor(all_labels_rep)
+
+        sim = input_doc_rep.squeeze() @ all_labels_rep.T
+        res = (torch.argsort(sim, dim=1)[:, -1]).numpy()
+        correct = np.array([all_labels.index(i) for i in true_val])
+        print(np.mean(res==correct))
+
+    return res
